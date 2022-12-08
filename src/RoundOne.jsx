bd7e08c896a17df37605198f68f2dfc7b2f4e926
Chris Blagg <blaggc@apple.com>
2022-12-08 15:50:19 -0800

    added game 2 questions

--- src/RoundOne.jsx
+++ src/RoundOne.jsx
@@ -1,26 +1,32 @@
-import QuestionChoice from "./QuestionChoice"
-import { useState, useEffect } from "react"
-import QuestionBlock from './QuestionBlock'
-import { roundOne } from './questions'
+import QuestionChoice from "./QuestionChoice";
+import { useState, useEffect } from "react";
+import QuestionBlock from "./QuestionBlock";
+import { roundOne } from "./questions";
 
-export default function RoundOne(){
+export default function RoundOne() {
+  const [question, setQuestion] = useState(false);
+  const [usedQuestions, setUsedQuestions] = useState([]);
 
-    const [question, setQuestion] = useState(false)
-    const [usedQuestions, setUsedQuestions] = useState([])
+  useEffect(() => {
+    if (question) {
+      setUsedQuestions([...usedQuestions, question]);
+    }
+  }, [question]);
 
-    useEffect(()=>{
-        if(question){
-            setUsedQuestions([...usedQuestions, question])
-        }
-    },[question])
-
-    return (
-        <div className="wall">
-            {
-            !question 
-            ? <QuestionChoice setQuestion={setQuestion} usedQuestions={usedQuestions} /> 
-            : <QuestionBlock question={question} setQuestion={setQuestion} questions={roundOne} />
-            }
-        </div>
-    )
+  return (
+    <div className="wall">
+      {!question ? (
+        <QuestionChoice
+          setQuestion={setQuestion}
+          usedQuestions={usedQuestions}
+        />
+      ) : (
+        <QuestionBlock
+          question={question}
+          setQuestion={setQuestion}
+          questions={roundOne}
+        />
+      )}
+    </div>
+  );
 }
